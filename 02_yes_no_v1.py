07797e7495f1b4434e135c40385a43b9c5e6b8c7
Casey Tanenui <78571630+CaseyTanenui@users.noreply.github.com>
2022-06-01 09:14:53 +1200

    Add files via upload
    Started 'yes/no' component

---
+++ 02_yes_no_v1.py
@@ -0,0 +1,17 @@
+# Checks that user has entered either 'yes' or 'no'...
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return var_item
+            elif response == var_item[0]:
+                return var_item
+                
+        print("Please enter either yes or no...\n")
